ca0d46a1d3e4cf75f923a8849c1bccee8ec199a6
iivanov <iivanov@ab-technology.ru>
2019-08-01 17:02:28 +0300

    Added service worker

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,5 +1,11 @@
+const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const ManifestPlugin = require('webpack-manifest-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
@@ -7,6 +13,35 @@ module.exports = merge(common, {
 	plugins: [
 		new webpack.DefinePlugin({
 			API_URL: JSON.stringify('https://xn--r38h.tk/api')
+		}),
+		new CopyWebpackPlugin([
+			{ from: 'src/manifest.json', to: path.join(__dirname, 'dist') }
+		]),
+		new WebpackAssetsManifest({
+			output: path.join(__dirname, 'dist/assets-manifest.json'),
+			merge: true
+		}),
+		new ImageminPlugin(),
+		new ManifestPlugin({
+			fileName: 'asset-manifest.json',
+		}),
+		exports.sw = new SWPrecacheWebpackPlugin({
+			// By default, a cache-busting query parameter is appended to requests
+			// used to populate the caches, to ensure the responses are fresh.
+			// If a URL is already hashed by Webpack, then there is no concern
+			// about it being stale, and the cache-busting can be skipped.
+			dontCacheBustUrlsMatching: /\.\w{8}\./,
+			filename: 'service-worker.js',
+			logger(message) {
+				if (message.indexOf('Total precache size is') === 0) {
+					// This message occurs for every build and is a bit too noisy.
+					return;
+				}
+				console.log(message);
+			},
+			minify: true, // minify and uglify the script
+			navigateFallback: '/index.html',
+			staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
 		})
 	],
 	optimization: {
